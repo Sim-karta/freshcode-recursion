486e02e249b7f3db5d2a13d274d55fd97e6b0ace
Sim-karta <simonenkonikita561@gmail.com>
2026-08-14 18:09:44 +0300

    Add value validation

--- assets/scripts/pow.js
+++ assets/scripts/pow.js
@@ -1,4 +1,16 @@
 function exponentiation(base, exponent) {
+    if (typeof base !== "number" || Number.isNaN(base)) {
+        throw new TypeError("base must be a number");
+    }
+
+    if (typeof exponent !== "number" || Number.isNaN(exponent)) {
+        throw new TypeError("exponent must be a number");
+    }
+
+    if (exponent < 0 || !Number.isSafeInteger(exponent)) {
+        throw new RangeError("exponent must be not negative and integer value");
+    }
+
     if (exponent === 1) {
         return base;
     }
@@ -8,7 +20,7 @@ function exponentiation(base, exponent) {
 
 console.log(
     exponentiation(
-        prompt("Введіть число яке хочете возвести"),
-        prompt("Введіть степінь"),
+        Number(prompt("Введіть число яке хочете возвести")),
+        Number(prompt("Введіть степінь")),
     ),
 );
